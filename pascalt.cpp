65da7979a7a819efc5a9066c84f82a5543255522
Prithivi-Raj <na19b052@smail.iitm.ac.in>
2021-09-20 12:44:09 +0530

    pasc

---
+++ pascalt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int pt(int a){
+    int x=1;
+    cout<<x<<" ";
+
+for (int i=0;i<a;i++){
+    curr=x*(a)
+
+}
+
+}
+int main(){
+    int h;
+    cout<<"enter ";
+    cin>>h;
+    pt(h);
+    return 0;
+
+}
